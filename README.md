8de36704fd8d42977a08eee938add10b62744003
robotomize <robotomize@gmail.com>
2024-01-30 10:10:52 +0300

    change token to env

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # tg-sender-docker
 ```shell
   docker build -t telegram-curl-bot . 
-  docker run --rm telegram-curl-bot TOKEN CHATID "test notification"
+  docker run --rm telegram-curl-bot -e TOKEN CHATID "test notification"
+  docker run --rm --env-file .env telegram-curl-bot CHATID "test notification"
 ```
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
-  echo "Usage: send_message.sh <TOKEN> <CHAT_ID> <TEXT>"
+if [ -z "$TOKEN" ] || [ -z "$1" ] || [ -z "$2" ]; then
+  echo "Usage: entrypoint.sh <CHAT_ID> <TEXT>"
   exit 1
 fi
 
-TOKEN=$1
-CHAT_ID=$2
-TEXT=$3
+CHAT_ID=$1
+TEXT=$2
 
 curl -vv -XPOST "https://api.telegram.org/bot$TOKEN/sendMessage" -H "Content-Type: application/json" -d "{\"chat_id\": \"$CHAT_ID\", \"text\": \"$TEXT\"}"
